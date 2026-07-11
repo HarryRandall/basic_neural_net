366081a5bba186b0e8d2c4d16608ee1ce2f57eb8
Harry Randall <randallhazza@gmail.com>
2026-07-11 14:04:48 +1000

    feat: setup basic training model using adam

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello world")
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+# flattens the input from a 28x28 grid into a 784 line of pixels.
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+# after a flatten layer, we want a dense layer
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+# output layer, ints 0-9, softmax is a pdf, returns an array of probs
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+
+model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10)
+
+model.save("handwritten.keras")
+
